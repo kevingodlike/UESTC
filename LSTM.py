8f00686349e30f20b09173921b3b33c6d5e941ec
data <42286245+kevingodlike@users.noreply.github.com>
2018-11-04 21:56:03 +0800

    Add files via upload

---
+++ LSTM.py
@@ -0,0 +1,72 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import os
+import numpy as np
+import random
+import string
+import tensorflow as tf
+import zipfile
+from six.moves import range
+from six.moves.urllib.request import urlretrieve
+
+filename = 'text8.zip'
+
+def read_data(filename):
+  f = zipfile.ZipFile(filename)
+  for name in f.namelist():
+    return tf.compat.as_str(f.read(name))
+  f.close()
+
+text = read_data(filename)
+print('Data size %d' % len(text))
+
+valid_size = 1000
+valid_text = text[:valid_size]
+train_text = text[valid_size:]
+train_size = len(train_text)
+print(train_size, train_text[:64])
+print(valid_size, valid_text[:64])
+
+vocabulary_size = len(string.ascii_lowercase) + 1 # [a-z] + ' '
+first_letter = ord(string.ascii_lowercase[0])
+
+def char2id(char):
+  if char in string.ascii_lowercase:
+    return ord(char) - first_letter + 1
+  elif char == ' ':
+    return 0
+  else:
+    print('Unexpected character: %s' % char)
+    return 0
+
+def id2char(dictid):
+  if dictid > 0:
+    return chr(dictid + first_letter - 1)
+  else:
+    return ' '
+
+print(char2id('a'), char2id('z'), char2id(' '), char2id('ï'))
+print(id2char(1), id2char(26), id2char(0))
+
+#把字符对应成数字
+def characters(probabilities):
+  """Turn a 1-hot encoding or a probability distribution over the possible
+  characters back into its (most likely) character representation."""
+  return [id2char(c) for c in np.argmax(probabilities, 1)]
+
+def batches2string(batches):
+  """Convert a sequence of batches back into their (most likely) string
+  representation."""
+  s = [''] * batches[0].shape[0]
+  for b in batches:
+    s = [''.join(x) for x in zip(s, characters(b))]
+  return s
+
+train_batches = BatchGenerator(train_text, batch_size, num_unrollings)
+valid_batches = BatchGenerator(valid_text, 1, 1)
+
+print(batches2string(train_batches.next()))
+print(batches2string(train_batches.next()))
+print(batches2string(valid_batches.next()))
+print(batches2string(valid_batches.next()))
